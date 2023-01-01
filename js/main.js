080c817d59a1fda6906f25c7c1edfc82386b8e8a
akbarjon07 <akbarjon.1019@gmail.com>
2023-01-01 12:31:12 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const elForm = document.querySelector(".form");
+const elInput = document.querySelector(".page-input");
+const elTypeSelect = document.querySelector(".type-select");
+const elPartSelect = document.querySelector(".part-select");
+const elText = document.querySelector(".num");
+
+const colorA5 = 100;
+const colorA4 = 200;
+const blackA5 = 60;
+const blackA4 = 120;
+
+elForm.addEventListener("change", (e) => {
+  e.preventDefault();
+
+  // if ((elTypeSelect.value = colorA5)) {
+  //   console.log(123);
+  // }
+});
